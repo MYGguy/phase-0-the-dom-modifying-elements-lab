c10d5413f316cb50a6f33799db85ab60f2914aef
Moshe Goldstein <mygguy613@gmail.com>
2024-07-19 17:50:44 -0400

    ct

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+
+// document.querySelector('h1').id = 'victory';
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Moshe is the champion';
+
+document.body.append(newHeader);
